f340fad39639f0f2bf9170862f2a5e8338893717
casetweezie <casey_cronin@yahoo.com>
2024-06-10 12:10:05 -0700

    links

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle); 
+
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle); 
